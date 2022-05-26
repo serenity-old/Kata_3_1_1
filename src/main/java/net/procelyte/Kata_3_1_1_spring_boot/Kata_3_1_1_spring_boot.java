fd247c2a10db0b35087dbd79e541db41f04bc1fc
Oleg <oleg-step@yandex.ru>
2022-05-26 22:30:31 +0300

    first commit

---
+++ src/main/java/net/procelyte/Kata_3_1_1_spring_boot/Kata_3_1_1_spring_boot.java
@@ -0,0 +1,13 @@
+package net.procelyte.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kata_3_1_1_spring_boot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kata_3_1_1_spring_boot.class, args);
+	}
+
+}
